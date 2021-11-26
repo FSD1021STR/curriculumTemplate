d4797f428f93c49565aaecfe16e81c8a51867760
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2021-11-26 21:36:38 +0100

    curriculum(WIP)

--- curriculum/src/App.js
+++ curriculum/src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 import Curriculum from "./curriculum";
-import Formulario from "./form";
+
 
 function App() {
+
   return (
     <div>
-      <Formulario />
-    </div>
+       <Curriculum/>
+    </div>   
   );
 }
 
--- curriculum/src/curriculum.js
+++ curriculum/src/curriculum.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "./curriculum.css";
+import logo from "./logo.svg";
 
 export default function Curriculum() {
   return (
     <div>
+      <div className="header">
+        <img src ={logo} width="50px" />
+        <h1>Curriculum</h1>
+      </div> 
+      <div>
+        <h3>Datos perosonales:</h3>
+        <hr />
+        <span>Nombre:</span>
+        <span className ="values">Alfonso</span>
+        <br />
+        <span>Apellido:</span>
+        <span className ="values">Fernández</span>
+        <br />
+      </div> 
     </div>
   );
 }
