91f9b0fe2d8c74a80218ae9fb7c576dc6829d781
JMeskini <jaume.meskini@gmail.com>
2021-11-26 18:14:31 +0100

    first commit

---
+++ curriculum/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Curriculum from "./curriculum";
+import Formulario from "./form";
+
+function App() {
+  return (
+    <div>
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
