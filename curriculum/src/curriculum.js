9f4588c1626ae97306ee747a8498cd5daf1a16a2
Juan <soraioniosif@gmail.com>
2021-11-26 21:33:03 +0100

    Se han modificadolos archivos

--- curriculum/src/curriculum.js
+++ curriculum/src/curriculum.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./curriculum.css";
-
+import logo from "./logo.svg";
 export default function Curriculum() {
   return (
     <div>
+      <div className="header">
+        <img src={logo} width="50px" />
+        <h1>Curriculum</h1>
+      </div>
+      <div>
+        <h3>Datos personales:</h3>
+        <hr />
+        <span>Nombre:</span>
+        <span className="values">Jaume</span>
+        <br />
+        <span>Apellido:</span>
+        <span className="values">Meskini</span>
+        <br />
+      </div>
     </div>
   );
 }
