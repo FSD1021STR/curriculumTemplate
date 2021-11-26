91a33e6f087611dc89da64baee4d9a1d8f114e2a
RamaManuel <manuelramabernal@gmail.com>
2021-11-26 21:36:06 +0100

    curriculum(WIP)

--- curriculum/src/curriculum.js
+++ curriculum/src/curriculum.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./curriculum.css";
+import logo from "./logo.svg";
 
 export default function Curriculum() {
   return (
     <div>
+      <div className="header">
+        <img src={logo} width="250px" />
+        <h1>Curriculum</h1>
+      </div>
+      <div>
+        <h3>Datos Personales</h3>
+        <hr />
+        <span>Nombre:</span>
+        <span className="values">Manuel</span>
+        <br />
+      </div>
     </div>
   );
 }
