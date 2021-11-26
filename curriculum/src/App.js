bab05b808c437badb4ef1ed4b0daacea7e9aa8e9
Juan <soraioniosif@gmail.com>
2021-11-26 21:32:35 +0100

    Se ha modificado el erchivo App.js

--- curriculum/src/App.js
+++ curriculum/src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Curriculum from "./curriculum";
-import Formulario from "./form";
+
+
+
 
 function App() {
   return (
     <div>
-      <Formulario />
+      <Curriculum />
     </div>
   );
 }
