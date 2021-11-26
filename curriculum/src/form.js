ec6e1125eb6ed631b487b66b7efde40cd9f424b4
JMeskini <jaume.meskini@gmail.com>
2021-11-26 18:15:46 +0100

    created dev

--- curriculum/src/form.js
+++ curriculum/src/form.js
@@ -3,19 +3,6 @@ import "./form.css";
 export default function Formulario() {
   return (
     <div>
-      <h1> Nuevo Usuario </h1>
-      <form>
-        <label>Nombre de usuario: </label>
-        <input placeholder="nombre de usuario" />
-        <br/>
-        <label>Contraseña: </label>
-        <input type="password" placeholder="password" />
-        <br/>
-        <label>Repite la contraseña: </label>
-        <input type="password" placeholder="password" />
-        <br/>
-        <input className="submit" type="submit" />
-      </form>
     </div>
   );
 }
